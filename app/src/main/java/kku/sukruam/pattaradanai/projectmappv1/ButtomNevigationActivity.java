ae55fbc3ba1778a834768f9857a9ad57d30b9c1c
DAI <dpatt_s@hotmail.com>
2018-04-19 02:41:24 +0700

    4

---
+++ app/src/main/java/kku/sukruam/pattaradanai/projectmappv1/ButtomNevigationActivity.java
@@ -0,0 +1,49 @@
+package kku.sukruam.pattaradanai.projectmappv1;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+public class ButtomNevigationActivity extends AppCompatActivity {
+
+
+    private Main MainFrangment;
+    private favFragment favorite;
+    private AccountFragment account;
+
+    private FrameLayout mainFrame;
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+
+                    return true;
+                case R.id.navigation_favorite:
+
+                    return true;
+                case R.id.navigation_account:
+
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buttom_nevigation);
+
+        mainFrame = (FrameLayout) findViewById(R.id.main_frame);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
