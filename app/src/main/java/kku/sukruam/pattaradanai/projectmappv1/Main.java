398979511cd293065d2aa8d9aa0b9b47ffd99020
DAI <dpatt_s@hotmail.com>
2018-04-18 19:37:32 +0700

    Initial commit

---
+++ app/src/main/java/kku/sukruam/pattaradanai/projectmappv1/Main.java
@@ -0,0 +1,36 @@
+package kku.sukruam.pattaradanai.projectmappv1;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+/**
+ * Created by MThai on 4/18/2018.
+ */
+
+public class Main extends Activity implements View.OnClickListener {
+    ImageButton IBtnMenu,IBtnCat,IBtnPop,IBtnAbout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        init();
+
+    }
+
+    private void init() {
+        IBtnMenu = (ImageButton) findViewById(R.id.ImgBtn_Menu);
+        IBtnCat = (ImageButton) findViewById(R.id.ImgBtn_catagory);
+        IBtnPop = (ImageButton) findViewById(R.id.ImgBtn_Pop);
+        IBtnAbout = (ImageButton) findViewById(R.id.ImgBtn_About);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
