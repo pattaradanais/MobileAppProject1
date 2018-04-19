5a8c7fc9f7d3bdd1eb612a433b3b776fc0c44fa4
DAI <dpatt_s@hotmail.com>
2018-04-19 19:33:36 +0700

    4

--- app/src/main/java/kku/sukruam/pattaradanai/projectmappv1/ButtomNevigationActivity.java
+++ app/src/main/java/kku/sukruam/pattaradanai/projectmappv1/ButtomNevigationActivity.java
@@ -4,36 +4,21 @@ package kku.sukruam.pattaradanai.projectmappv1;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.FrameLayout;
+
 
 public class ButtomNevigationActivity extends AppCompatActivity {
 
 
-    private MainFragment mainFragment;
+    /*private MainFragment mainFragment;
     private favFragment favorite;
     private AccountFragment account;
 
-    private FrameLayout mainFrame;
+    private FrameLayout mainFrame;  */
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_buttom_nevigation);
-
-        mainFrame = (FrameLayout) findViewById(R.id.main_frame);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-
-        mainFragment = new MainFragment();
-        favorite = new favFragment();
-        account = new AccountFragment();
-
-        setFragment(mainFragment);
-    }
 
 
 
@@ -45,31 +30,56 @@ public class ButtomNevigationActivity extends AppCompatActivity {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    setFragment(mainFragment);
+                    transaction.replace(R.id.main_frame,new MainFragment()).commit();
+                    //setFragment(mainFragment);
                     return true;
                 case R.id.navigation_favorite:
-                    setFragment(favorite);
+                    transaction.replace(R.id.main_frame, new favFragment()).commit();
+                    //setFragment(favorite);
                     return true;
                 case R.id.navigation_account:
-                    setFragment(account);
+                    transaction.replace(R.id.main_frame,new AccountFragment()).commit();
+                   // setFragment(account);
                     return true;
 
-                        default:
-                            return false;
+
+
             }
+            return false;
 
         }
 
 
     };
 
-    private void setFragment(Fragment fragment) {
 
-        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buttom_nevigation);
+
+       // mainFrame = (FrameLayout) findViewById(R.id.main_frame);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+       /* mainFragment = new MainFragment();
+        favorite = new favFragment();
+        account = new AccountFragment();
+  */
+
+    }
+
+
+  /*  private void setFragment(Fragment fragment) {
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+           FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.main_frame, fragment);
         fragmentTransaction.commit();
-    }
+    }  */
 };
 
